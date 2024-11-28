a516194b1b7aa67fda06a6d24ac258f35459f6f6
ejembi22 <ejembi_emma@icloud.com>
2024-11-28 04:09:36 -0800

    third-commit

--- src/Header.js
+++ src/Header.js
@@ -10,16 +10,16 @@ const Header = () => {
         
   <div class="container-fluid">
     {/* <a class="navbar-brand" href="#">Navbar</a> */}
-    <img src={Logo} alt="logo" className='px-4 bbb'/>
+    <img src={Logo} alt="logo" className='px-5 bbb'/>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <div class="collapse navbar-collapse navbaritems" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
           <a class="nav-link active" aria-current="page" href="#">Home</a>
         </li>
-        <li class="nav-item">
+        <li class="nav-item ">
           <a class="nav-link aboutus" href="#">About us</a>
         </li>
         <li class="nav-item">
@@ -33,17 +33,17 @@ const Header = () => {
             Contact Us
           </a>
           <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><a class="dropdown-item" href="#">Email</a></li>
+            <li><a class="dropdown-item" href="#">Phone</a></li>
             <li><hr class="dropdown-divider"></hr></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
+            <li><a class="dropdown-item" href="#">Address</a></li>
           </ul>
         </li>
         
       </ul>
       <div class="d-flex conandgetbtn ">
-      <button class="btn btn-outline-success me-4  border-warning text-warning button1" type="clickable">Contact us</button>
-        <button class="btn btn-outline-success  button1 bg-warning text-dark border-warning" type="clickable">Get started</button>
+      <button class="btn btn-outline-success me-2  border-warning text-warning button1 fw-bolder" type="clickable">Contact us</button>
+        <button class="btn btn-outline-success  button1 bg-warning text-dark border-warning fw-bolder" type="clickable">Get started</button>
       </div>
     </div>
   </div>
